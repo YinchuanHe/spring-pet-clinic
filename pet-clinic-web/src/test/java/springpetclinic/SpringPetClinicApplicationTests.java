5eaead540400d9df4026f8b380df5747741b5a61
YinchuanHe <yhe023@ucr.edu>
2019-08-26 16:51:07 -0700

    Added Maven Release Plugin. close #3

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,12 +1,13 @@
 package springpetclinic;
 
+import com.springproject.springpetclinic.SpringPetClinicApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {SpringPetClinicApplication.class})
 public class SpringPetClinicApplicationTests {
 
     @Test
