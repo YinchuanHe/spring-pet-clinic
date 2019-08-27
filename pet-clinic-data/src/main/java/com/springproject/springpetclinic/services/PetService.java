08954c2db3cd6d1cc0490d9ceb0d74c5c47cef6b
YinchuanHe <yhe023@ucr.edu>
2019-08-26 22:33:59 -0700

    Created Interfaces for Pet Clinic Services. close #5

---
+++ pet-clinic-data/src/main/java/com/springproject/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.springproject.springpetclinic.services;
+
+import com.springproject.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/springproject/springpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.springproject.springpetclinic.services;
+
+import com.springproject.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
