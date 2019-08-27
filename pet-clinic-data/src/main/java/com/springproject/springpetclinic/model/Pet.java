a9d6e8c1f24473e24d61efd8ae5ab0230f51f480
YinchuanHe <yhe023@ucr.edu>
2019-08-26 23:06:37 -0700

    Added ID to all models. close #10

--- pet-clinic-data/src/main/java/com/springproject/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springproject/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.springproject.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private  Person owner;
