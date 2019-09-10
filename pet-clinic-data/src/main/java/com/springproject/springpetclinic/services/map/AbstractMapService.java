8aef0e9ab2c15059c11de5dd60a4b1a6fe181ed5
root <root@Surface.localdomain>
2019-09-10 14:57:36 -0700

    Refactored Map Impl to abstruct ID Generation. closes#23

--- pet-clinic-data/src/main/java/com/springproject/springpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springproject/springpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package com.springproject.springpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import com.springproject.springpetclinic.model.BaseEntity;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +20,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -30,5 +40,9 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
         //Require a proper equals method for entity
+    }
+
+    private Long getNextId(){
+        return Collections.max(map.keySet()) + 1;
     }
 }
